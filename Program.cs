45a0861cf7720d8e93b73e8af08af6bfbc9e317a
ИИП ТТ <122653888+Urruguay@users.noreply.github.com>
2023-03-23 12:13:47 +0200

    2 sposob

--- Program.cs
+++ Program.cs
@@ -11,20 +11,21 @@ namespace ConsoleApp10
         static void Main(string[] args)
         {
             //1 сортировка ОМ по адресу 
-            int[] nums = {12,-76,-6,-34, 1};
-            for (int i = 0; i < nums.Length - 1; i++)
-            {
-                for(int j = i+1; j<nums.Length; j++)
-                {
-                    if (nums[i] > nums[j])
-                    {
-                        int p = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = p;
-                    }
-                }
-            }
-            foreach (int i in nums)
+            int[] array = {12,-76,-6,-34, 1};
+            //for (int i = 0; i < nums.Length - 1; i++)
+            //{
+            //    for(int j = i+1; j<nums.Length; j++)
+            //    {
+            //        if (nums[i] > nums[j])
+            //        {
+            //            int p = nums[i];
+            //            nums[i] = nums[j];
+            //            nums[j] = p;
+            //        }
+            //    }
+            //}
+            Array.Sort(array);
+            foreach (int i in array)
             {
                 Console.WriteLine($"{i}");
             }
